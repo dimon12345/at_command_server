7a7e8dfcb8115311e568cdded2ee86a37608d073
q q <q@q.com>
2026-09-16 21:56:03 +0700

    Empty declaration removed

--- src/platform/TtyTerminal.h
+++ src/platform/TtyTerminal.h
@@ -12,7 +12,6 @@ public:
     size_t readData(std::span<char> buffer) const;
     size_t writeData(std::string_view data) const;
     int bytesAvailable() const;
-    void sendAtCommandResponse(std::string_view response);
 private:
     UniqueFileDescriptor tty_master_fd_;
     std::string tty_slave_name_;
