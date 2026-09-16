2ecf4b2e118065c0c30ed76c59913e71356fda01
q q <q@q.com>
2026-09-16 13:26:48 +0700

    AtCommandHandler interface cleanded

--- src/server/AtCommandHandler.h
+++ src/server/AtCommandHandler.h
@@ -6,7 +6,6 @@ class AtCommandHandler {
 public:
     AtCommandHandler(class AtCommandResponseSender &sender) : sender_(sender) {}
     virtual ~AtCommandHandler() = default;
-    virtual void handle(std::string_view command) = 0;
 
 protected:
     class AtCommandResponseSender &sender_;
--- src/server/command_handlers/CpinCommandHandler.h
+++ src/server/command_handlers/CpinCommandHandler.h
@@ -7,8 +7,7 @@ public:
     CpinCommandHandler(class AtCommandResponseSender &sender, class Modem &modem)
         : AtCommandHandler(sender), modem_(modem) {}
     void handleStatusCommand();
-
-    void handle(std::string_view command) override;
+    void handle(std::string_view command);
 private:
     class Modem &modem_;
 };
